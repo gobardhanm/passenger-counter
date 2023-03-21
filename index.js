45ce82044f76d5cb0e3dd7d975011742d04cf373
gobardhanm <gobardhanms364@gmail.com>
2023-03-21 23:23:33 +0530

    passenger-counter added

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-let myAge = 35;
-let dogAge = 7;
-let myDogAge = myAge * dogAge;
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
-console.log(myDogAge);
-console.log(myDogAge);
+let count = 0;
+
+function increment(){
+
+    count += 1;
+    countEl.textContent = count
+}
+
+function save(){
+    let entries = count + " - "
+    saveEl.textContent += entries
+    console.log(count)
+    countEl.textContent = 0
+    count = 0
+
+}
